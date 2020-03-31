52a1c03c91fd0acdff56d5ccb57d2ca5ea560a81
gregorix <gregorixpd0809@gmail.com>
2020-03-31 16:07:59 -0400

    FactoryMethod

---
+++ Creador.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+   public class Creador
+    {
+        public const int VINO_TINTO = 1;
+        public const int CERVEZA = 2;
+
+        public static BebidaEmbriagante CreadorBebidas(int Tipo)
+        {
+            switch (Tipo)
+            {
+                case VINO_TINTO:
+                    return new VinoTinto();
+
+                case CERVEZA:
+                    return new Cerveza();
+
+                default: return null;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BebidaEmbriagante oBebida = Creador.CreadorBebidas(Creador.CERVEZA);
+            Console.WriteLine("Cerveza:");
+            Console.WriteLine(oBebida.CuantoMeEmbriagaPorHora());
+
+            Console.ReadKey();
+
+            BebidaEmbriagante bebida = Creador.CreadorBebidas(Creador.VINO_TINTO);
+            Console.WriteLine("Vino Tinto:");
+            Console.WriteLine(bebida.CuantoMeEmbriagaPorHora());
+
+            Console.ReadKey();
+        }
+    }
+}
